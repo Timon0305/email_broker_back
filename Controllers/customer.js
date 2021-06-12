9472ea24f2b72ac0845b6cbf4e8cf25c055c6152
Timon0305 <timon0305@outlook.com>
2021-06-12 16:06:27 -0400

    path fixed

--- Controllers/customer.js
+++ Controllers/customer.js
@@ -64,7 +64,7 @@ exports.fileUpload = (req, res) => {
         user.save();
     })
     if (req.files.file) {
-        let path = __dirname + '\\..\\public\\' + req.files.file.name;
+        let path = __dirname + '/../public/' + req.files.file.name;
         console.log('=>>>>>>>>>>>', path)
         req.files.file.mv(path, (err) => {
             if (err) {
